5d0073064323b5ec48494ff28b58fee5da754970
eranroz <eranroz89@gmail.com>
2021-12-02 08:12:29 +0200

    Aligning to newer pywikibot

--- label_newly_connected.py
+++ label_newly_connected.py
@@ -3,17 +3,19 @@ Update wikidata labels based on recently articles connected to wikidata
 """
 import datetime
 import pywikibot
+from pywikibot import pagegenerators
 
 local_wiki = pywikibot.Site()
 wikidata = local_wiki.data_repository()
 wikidata.login()
-st = local_wiki.getcurrenttime()
-
-wd_changes = wikidata.recentchanges(changetype='edit', namespaces=[0], showBot=False,
+#st = local_wiki.getcurrenttime()
+st = local_wiki.server_time()
+wd_changes = wikidata.recentchanges(changetype='edit', namespaces=[0], bot=False,
                                     start=st, end=st-datetime.timedelta(days=1))
 new_local_sitelinks = (pywikibot.ItemPage(wikidata, e['title']) for e in wd_changes if e['comment'].startswith('/* wbsetsitelink-add:1|hewiki'))
-
-for data_item in wikidata.preloaditempages(new_local_sitelinks):
+# preload
+new_local_sitelinks = pagegenerators.PreloadingEntityGenerator(new_local_sitelinks)
+for data_item in new_local_sitelinks:
     if local_wiki.lang in data_item.labels or local_wiki.dbName() not in data_item.sitelinks:
         continue
     local_page = pywikibot.Page(local_wiki, data_item.getSitelink(local_wiki))
@@ -27,7 +29,7 @@ for data_item in wikidata.preloaditempages(new_local_sitelinks):
             'id': data_item.title(),
             'value': he_title,
             'language': local_wiki.lang,
-            'token': wikidata.token(None, 'csrf')
+            'token': wikidata.tokens['csrf']
         })).submit()
-    except pywikibot.data.api.APIError as e:
+    except pywikibot.exceptions.APIError as e:
         continue
